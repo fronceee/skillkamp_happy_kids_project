75e45a2461428fffe3e1db0e1ed34581ab9b5cc1
fronceee <tharathip.tamwiset@gmail.com>
2023-04-06 01:13:19 +0700

    update Carousel component's functionality

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -4,13 +4,22 @@ import { AnimatePresence, motion } from "framer-motion";
 export default function Carousel() {
   const [images, setImages] = React.useState([]);
   const [currentIndex, setCurrentIndex] = React.useState(0);
-  const [progress, setProgress] = React.useState(0);
 
+  function ProgressDots({ index }) {
+    const dotStyle = "w-3 h-3 bg-main-1 rounded-lg";
+    return (
+      <div className="flex gap-4">
+        {images.map((item, dotIndex) => (
+          <div className={`${dotStyle} ${dotIndex === index ? "opacity-80" :  "opacity-40"}`}></div>
+        ))}
+      </div>
+    );
+  }
   function ImageCarousel({ src }) {
     return (
-      <AnimatePresence className="w-screen h-full">
+      <AnimatePresence className="w-screen h-full select-none">
         <motion.div
-          className="w-full h-96 lg:h-[600px]"
+          className="w-full h-[300px] lg:h-[600px]"
           initial={{ opacity: 0.5 }}
           animate={{ opacity: 1 }}
           exit={{ opacity: 0 }}
@@ -22,21 +31,21 @@ export default function Carousel() {
     );
   }
 
-  function goNext() {
-    setCurrentIndex(prev => prev += 1);
-
+  const goNext = (e) => {
+    e.preventDefault();
+    setCurrentIndex((prev) => (prev += 1));
     if (currentIndex === images.length - 1) {
-        setCurrentIndex(0)
+      setCurrentIndex(0);
     }
-  }
+  };
 
-  function goBack() {
-    setCurrentIndex(prev => prev -= 1);
+  const goBack = (e) => {
+    e.preventDefault();
+    setCurrentIndex((prev) => (prev -= 1));
     if (currentIndex === 0) {
-        setCurrentIndex(images.length - 1)
+      setCurrentIndex(images.length - 1);
     }
-  }
-
+  };
 
   React.useState(() => {
     const controller = new AbortController();
@@ -63,10 +72,17 @@ export default function Carousel() {
   }, [currentIndex]);
 
   return (
-    <div>
-      <button onClick={() => goBack()}>Back</button>
+    <div className="relative w-screen">
+      <button onClick={goBack} className="absolute left-[5%] top-1/2 z-10">
+        Back
+      </button>
       <ImageCarousel src={images[currentIndex]} />
-      <button onClick={() => goNext()}>Next</button>
+      <div className="flex justify-center relative bottom-10">
+        <ProgressDots index={currentIndex} />
+      </div>
+      <button onClick={goNext} className="absolute right-[5%] top-1/2">
+        Next
+      </button>
     </div>
   );
 }
